c36ddd7211b279f54442433cd7409a95787d8be1
Mohamedmv22 <vevomohamed0@gmail.com>
2025-12-14 22:41:26 +0200

    Added alx_book_store.sql

---
+++ db.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="MohamedVevo12#",
+        database="my_db",
+        port=3306,
+        use_pure=True
+    )
